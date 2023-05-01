a075590d1857a96d0924c5ce234e802020ba2f02
khk811 <hyunkyungkim811@gmail.com>
2023-05-01 20:39:35 +0900

    MISC: error 체크용 echo 추가

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Wordpress download : 만약 index.php가 없는 상태라면 다운로드 함
-if [ ! -e index.php ]
-then
-	echo "Download wordpress\n";
-	wp core download --locale=ko_KR --allow-root
-fi
+# if [ ! -e index.php ]
+#then
+#	echo "Download wordpress\n";
+#	wp core download --locale=ko_KR --allow-root
+#fi
 
 # wp-config.php 생성 (그리고 install?)
-if [ ! -e wp-config.php ]
+if [ -e wp-config.php ]
 then
 	echo "create config\n" && \
 	wp config create --dbname=wp_hyunkkim --dbuser=hyunkkim --dbpass=1234 --allow-root && \
@@ -24,6 +24,8 @@ then
 	--admin_email=hyunkkim@student.42seoul.kr --allow-root
 fi
 
+exec /bin/sh
+
 # open wp-admin in browser
-echo "open wordpress admin in browser\n";
-wp admin --allow-root
+# echo "open wordpress admin in browser\n";
+# wp admin --allow-root
