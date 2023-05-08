14a3adab8ae388ad4eb9574399e57b900523e2d8
khk811 <hyunkyungkim811@gmail.com>
2023-05-08 20:17:02 +0900

    ADD: mariadb 스크립트 추가, 도커파일과 설정파일 변경

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 
-mysqld --user=root
+chmod -R 777 /var/lib/mysql
+chown -R mysql:mysql /var/lib/mysql
+
+
+touch /tmp/mysql.sock
+chmod 777 /tmp/mysql.sock
+
+# ln -s /tmp/mysql.sock /var/mysql/mysql.sock
+
+# sudo ufw allow 3306
+# nmap localhost
+
+sudo service mariadb status
+sudo service mariadb start
+sudo service mariadb status
+
+# mysql -e "CREATE DATABASE $MYSQL_DATABASE IF NOT EXISTS"
+
+mysqladmin -u $MYSQL_USER -p $MYSQL_PASSWORD
+
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE IF NOT EXISTS; \
+grant all privileges $MYSQL_DATABASE.* to $MYSQL_USER@'%' identified by $MYSQL_PASSWORD;\
+flush privileges;"
+
+# mysqld --user=root
