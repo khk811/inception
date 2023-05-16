50fef67c29dbfe089e76566b0edc9788b274aeaa
khk811 <hyunkyungkim811@gmail.com>
2023-05-16 21:42:49 +0900

    MISC: root 비밀번호를 임시로 통일함

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -21,11 +21,11 @@ echo "------DB CREATE DONE----------\n"
 # echo "------CHECK DB STATUS----------\n"
 # service mariadb status
 echo "------RESET ROOT PASSWORD----------\n"
-mysql -uroot -e "ALTER USER root@localhost IDENTIFIED BY 'r1234'; FLUSH PRIVILEGES;"
+mysql -uroot -e "ALTER USER root@localhost IDENTIFIED BY '1234'; FLUSH PRIVILEGES;"
 # echo "------CHECK DB STATUS----------\n"
 # mysqladmin -uroot -pr1234 status
 echo "------SHUTDOWN----------\n"
-mysqladmin -uroot -pr1234 shutdown
+mysqladmin -uroot -p1234 shutdown
 # mysqladmin shutdown
 
 echo "------MYSQLD----------\n"
