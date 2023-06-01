549b369aea010add385abbc2fea77a38077fe05a
khk811 <hyunkyungkim811@gmail.com>
2023-06-01 19:10:35 +0900

    WIP: please

--- srcs/requirements/bonus/static_web/tools/static_web_script.sh
+++ srcs/requirements/bonus/static_web/tools/static_web_script.sh
@@ -4,9 +4,9 @@ if [ ! -e /data/static-web/index.html ]; then
 	echo "COPY WEB FILE\n";
 	mkdir -p /data
 	cp -R /tmp/static-web /data/
-	ls -la /data && ls -la /data/staic-web/
+	ls -la /data && ls -la /data/static-web/
 else
 	echo "webpage already exists\n"
 fi
 
-exec /usr/sbin/nginx -g daemon off
+exec /usr/sbin/nginx -g "daemon off"
